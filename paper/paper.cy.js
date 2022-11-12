6f7f4e27da098d55a54ea8c02a4b489e94cd9dbc
hchriastian <55739504+hchriastian@users.noreply.github.com>
2022-11-12 07:56:02 +0700

    update folder

---
+++ paper/paper.cy.js
@@ -0,0 +1,25 @@
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://paper.id/webappv1/#/login')
+    })
+  
+    // https://on.cypress.io/interacting-with-elements
+  
+    it('.type() - type into a DOM element', () => {
+      // https://on.cypress.io/type
+      cy.get('.auth-input-container')
+        .type('kandidat@paper.id')
+
+      cy.get('.auth-form__submit-btn')
+        .click({ force: true })
+
+      cy.get('.auth-mb-10')
+        .type('jakarta123')
+
+      cy.get('.auth-form__submit-btn')
+        .click({ force: true })
+       
+
+
+    })
+})
